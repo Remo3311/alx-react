559aee3d8ef2286d601d01c36ce0c5f55f93cb5a
Remo3311 <remo33553311@gmail.com>
2024-08-06 15:38:40 +0000

    task_0

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,51 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-
-const rowStyle = {
-  backgroundColor: "#f5f5f5ab",
-};
-
-const headerStyle = {
-  backgroundColor: "#deb5b545",
-};
-
-function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
-  return (
-    <tr style={rowStyle}>
-      {isHeader ? (
-        textSecondCell === null ? (
-          <th style={headerStyle} colSpan={2}>
-            {textFirstCell}
-          </th>
-        ) : (
-          <>
-            <th style={headerStyle}>{textFirstCell}</th>
-            <th style={headerStyle}>{textSecondCell}</th>
-          </>
-        )
-      ) : (
-        <>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
-        </>
-      )}
-    </tr>
-  );
-}
-
-CourseListRow.propTypes = {
-  isHeader: PropTypes.bool,
-  textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
-
-CourseListRow.defaultProps = {
-  isHeader: false,
-  textSecondCell: null,
-};
-
-export default CourseListRow;
-import React from "react";
 import { shallow } from "enzyme";
 import CourseListRow from "./CourseListRow";
 
@@ -53,7 +6,8 @@ describe("Testing <CourseListRow />", () => {
   it("When isHeader is true test the component renders one cell with colspan = 2 when textSecondCell does not exist", () => {
     let wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="first cell test"/>);
     expect(wrapper.find('th')).toHaveLength(1);
-    expect(wrapper.find('th').prop('colSpan')).toEqual(2); // Ensure colSpan is a number
+    // Change '2' to 2 to match the number type
+    expect(wrapper.find('th').prop('colSpan')).toEqual(2);
     expect(wrapper.find('th').text()).toEqual('first cell test');
   });
 
