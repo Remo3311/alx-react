09573c2ea2b104b86ab2c325bae0acbd700612bf
Remo3311 <remo33553311@gmail.com>
2024-08-11 15:30:04 +0000

    task_0

--- 0x05-react_state/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-react_state/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   const headerStyle = {backgroundColor: '#deb5b545'};
@@ -10,21 +11,39 @@ function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
     <tr style={selected_style}>
       {isHeader ?
         textSecondCell === null ?
-          <th colSpan="2">{textFirstCell}</th>
+          <th colSpan="2" className={css(rowsStyles.thcenter)}>{textFirstCell}</th>
         :
           <>
-            <th>{textFirstCell}</th>
-            <th>{textSecondCell}</th>
+            <th className={css(rowsStyles.th)}>{textFirstCell}</th>
+            <th className={css(rowsStyles.th)}>{textSecondCell}</th>
           </>
       :
         <>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
+          <td className={css(rowsStyles.td)}>{textFirstCell}</td>
+          <td className={css(rowsStyles.td)}>{textSecondCell}</td>
         </>
       }
     </tr>
   );
 }
+
+const rowsStyles = StyleSheet.create({
+  thcenter: {
+    borderBottom: '1px solid gray',
+    margin: 0,
+    padding: 0,
+    textAlign: 'center'
+  },
+  th: {
+    borderBottom: '1px solid gray',
+    margin: 0,
+    padding: 0,
+    textAlign: 'left'
+  },
+  td: {
+    paddingLeft: 3
+  }
+});
 
 CourseListRow.defaultProps = {
   isHeader: false,
